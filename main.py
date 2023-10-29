c4ddb495dc303a9dc7a17c265ce8d8e263f848a9
Rafa <galmaw@hotmail.com>
2023-10-29 21:10:07 +0100

    make main.py readable

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from wordref.wordref import Wordref
 from gr_datetime.gr_date import get_full_date
 
 
-config = dotenv_values('.env')
+config = dotenv_values(".env")
 
 
 class MyClient(discord.Client):
@@ -19,7 +19,7 @@ class MyClient(discord.Client):
         if not self.synced:
             await tree.sync()
             self.synced = True
-        print(f'Bot is ready! {self.user}')
+        print(f"Bot is ready! {self.user}")
 
     async def on_message(self, message):
         if message.author == self.user:
@@ -28,20 +28,14 @@ class MyClient(discord.Client):
 
 intents = discord.Intents.default()
 intents.message_content = True
-
 client = MyClient(intents)
 tree = app_commands.CommandTree(client)
 
-# Base command. Prompts a random word entry from wordref.
-@tree.command(
-    name="wotdgr",
-    description="Prompts a random Greek word entry from Wordref"
-)
+
+@tree.command(name="wotdgr", description="Prompts a random Greek word from Wordref")
 async def self(interaction: discord.Interaction):
     try:
         embedded_msg = Wordref(word=None, GrEn=True).embed()
-    except requests.exceptions.RequestException as e:
-        embedded_msg = f"Request Exception: {str(e)}"
     except Exception as e:
         embedded_msg = f"Error: {str(e)}"
     finally:
@@ -51,51 +45,37 @@ async def self(interaction: discord.Interaction):
             await interaction.response.send_message(content=embedded_msg)
 
 
-
-# Prompts a random word entry from wordref but from English to Greek.
-@tree.command(
-    name="wotden",
-    description="Prompts a random english word entry from wordref")
+@tree.command(name="wotden", description="Prompts a random english word from Wordref")
 async def self(interaction: discord.Interaction):
-    await interaction.response.send_message(embed=Wordref(word=None, GrEn=False).embed())
+    await interaction.response.send_message(
+        embed=Wordref(word=None, GrEn=False).embed()
+    )
 
 
-@tree.command(
-    name="searchgr",
-    description="Searches for the given greek word in wordref")
+@tree.command(name="searchgr", description="Searches the given greek word in Wordref")
 async def self(interaction: discord.Interaction, word: str):
     await interaction.response.send_message(embed=Wordref(word=word, GrEn=True).embed())
 
 
-@tree.command(
-    name="searchen",
-    description="Prompts a random word entry from wordref")
+@tree.command(name="searchen", description="Searches the given english word in Wordref")
 async def self(interaction: discord.Interaction, word: str):
-    await interaction.response.send_message(embed=Wordref(word=word, GrEn=False).embed())
+    await interaction.response.send_message(
+        embed=Wordref(word=word, GrEn=False).embed()
+    )
 
 
-
-
-# https://plainenglish.io/blog/send-an-embed-with-a-discord-bot-in-python
-@tree.command(
-    name="helprafa",
-    description="Explains rafabot")
+@tree.command(name="helprafa", description="Explains rafabot")
 async def self(interaction: discord.Interaction):
     embed = discord.Embed(
-        title="Explains rafabot",
-        # url="https://realdrewdata.medium.com/",
-        description=HelpMessage().message,
-        color=0xFF5733
+        title="Explains rafabot", description=HelpMessage().message, color=0xFF5733
     )
     await interaction.response.send_message(embed=embed)
 
-# Dates
-@tree.command(
-    name="date",
-    description="Prompts date in Fidis format")
+
+@tree.command(name="date", description="Prompts date in Fidis format")
 async def self(interaction: discord.Interaction):
     await interaction.response.send_message(get_full_date())
 
 
-if __name__ == '__main__':
-    client.run(config['TOKEN'])
+if __name__ == "__main__":
+    client.run(config["TOKEN"])
